0be46643ddd439668b5da305e014cfa9f3330736
Ravi Ranjan <127501852+RR0312@users.noreply.github.com>
2024-05-26 10:50:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let msg = new SpeechSynthesisUtterance();
+let voices= SpeechSynthesis.getVoices();
+msg.voice=voices[0];
+let tags= document.querySelectorAll('p,a,h1,h2,h3,div,span,table,h6,div');
+tags.forEach((tag) => {
+    msg.text=e.target.innerText;
+    tag.computedStyleMap.backgroundColor="yellow";
+    SpeechSynthesis.speak(msg);
+    let interval=setInterval(() => {
+        if(!speechSynthesis.speaking){
+            tag.computedStyleMap.removeProperty{'background-color'};
+            clearInterval(interval);
+        }
+    }, 100);
+});
